87c3a15cae1677fc913b411d5f82769556657425
omar.loaiza <omar.loaiza@FIA-LAB7A1.lim.upeu.edu.pe>
2014-02-20 10:56:10 -0500

    Segundo envio

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          System.out.println("Hola Mundo Lima");        // TODO code application logic here
     }
     
 }
