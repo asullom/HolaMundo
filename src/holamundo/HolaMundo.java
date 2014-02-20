bcd91f4671cd4fe6d6df61dd1eaec17c5a125aed
Angel Sullon <asullom@gmail.com>
2014-02-20 12:07:55 -0500

    3er envio: es la tarea 201

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -17,7 +17,8 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.err.println("Hola Mundo Java");
+        System.out.println("Hola Mundo Java");
+        //Hola soy Angel
     }
     
 }
